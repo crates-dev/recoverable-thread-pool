17abdab40fb5e717c8c7b654be6015c098601fea
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:38 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/recoverable-thread-pool.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/recoverable-thread-pool.git;
 git remote add origin git@github.com:ltpp-universe/recoverable-thread-pool.git;
