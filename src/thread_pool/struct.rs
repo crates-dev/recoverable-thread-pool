9a29287b0b13732060433b0c785ffa46fe40476a
尤雨东 <root@ltpp.vip>
2025-07-30 22:18:46 +0800

    feat: v2.4.1

--- src/thread_pool/struct.rs
+++ src/thread_pool/struct.rs
@@ -4,10 +4,23 @@ use crate::*;
 ///
 /// Manages a collection of worker threads and provides methods
 /// to submit tasks for execution.
+///
+/// # Returns
+///
+/// - `ThreadPool` - A new thread pool instance.
+#[derive(Debug)]
 pub struct ThreadPool {
     /// The collection of worker threads.
+    ///
+    /// # Returns
+    ///
+    /// - `Vec<Worker>` - The collection of worker threads.
     #[allow(dead_code)]
     pub(super) workers: Vec<Worker>,
     /// The sender channel for submitting jobs to workers.
+    ///
+    /// # Returns
+    ///
+    /// - `Sender<ThreadPoolJob>` - The sender channel for submitting jobs to workers.
     pub(super) sender: Sender<ThreadPoolJob>,
 }
