98a1f08fec9511c0a658d4664e684ddb94a8a543
尤雨东 <root@ltpp.vip>
2025-10-19 12:19:35 +0800

    feat: v2.4.6

--- src/worker/impl.rs
+++ src/worker/impl.rs
@@ -22,14 +22,14 @@ impl Worker {
         spawn(|| {
             let _ = sync::recoverable_spawn(move || {
                 loop {
-                    if let Ok(receiver_lock) = receiver.lock() {
-                        if let Ok(job) = receiver_lock.recv() {
-                            let _ = sync::recoverable_spawn(job);
-                        }
+                    if let Ok(receiver_lock) = receiver.lock()
+                        && let Ok(job) = receiver_lock.recv()
+                    {
+                        let _ = sync::recoverable_spawn(job);
                     }
                 }
             });
         });
-        return Some(Worker { id });
+        Some(Worker { id })
     }
 }
